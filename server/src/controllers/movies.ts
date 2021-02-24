8d3dd97040efdbad183cc832d2c2ae6d0fe1474b
zakordonetss <60739399+zakordonetss@users.noreply.github.com>
2021-02-24 20:56:12 +0200

    created sorting and filtering methods

--- server/src/controllers/movies.ts
+++ server/src/controllers/movies.ts
@@ -6,6 +6,17 @@ moviesService.start();
 
 export const MoviesController = Router()
     .get('/', (req, res) => {
-        const json = moviesService.read();
-        res.status(200).send(json);
+        const movies = moviesService.read();
+        res.status(200).send(movies);
+    })
+    .put('/sort/year/:info', (req, res) => {
+        // info === descending or ascending
+        const { info } = req.params;
+        const movies = moviesService.sortByYears(info);
+        res.status(201).send(movies);
+    })
+    .put('/filter/years/:year', (req, res) => {
+        const { year } = req.params;
+        const movies = moviesService.filterByYear(year);
+        res.status(201).send(movies);
     })
--- server/src/services/MoviesServise.ts
+++ server/src/services/MoviesServise.ts
@@ -7,21 +7,80 @@ const csv = csvtojson();
 export class MoviesService {
     private readonly _csvFilePath = resolve(__dirname, '../data', 'movies22.csv');
     private _movies: IMovie[];
+    private _genres: string[];
 
-    public start(): void {
-        this._parserFiterCsv();
+    public async start(): Promise<void> {
+        this._setMoviesData();
     }
 
     public read(): IMovie[] {
         return this._movies;
     }
 
-    private _parserFiterCsv(): void {
+    public sortByYears(info: string): IMovie[] {
+        try {
+            this._movies.sort((a, b) => {
+                const first = a.year;
+                const second = b.year;
+
+                if (typeof first === 'string' && typeof second === 'string') {
+                    switch (info) {
+                        case 'descending':
+                            return second.localeCompare(first);
+                        case 'ascending':
+                            return first.localeCompare(second);
+                        default: throw new Error("Invalid sorting info");
+                    }
+                }
+                if (typeof first === 'number' && typeof second === 'number') {
+                    switch (info) {
+                        case 'descending':
+                            return second - first;
+                        case 'ascending':
+                            return first - second;
+                        default: throw new Error("Invalid sorting info");
+                    }
+                }
+            })
+            return this._movies;
+
+        } catch(err) {
+            if (err instanceof Error) throw err;
+            else throw new Error("Error during sorting");
+        }
+    }
+
+    public filterByYear(year: string): IMovie[] {
+        try {
+            const filmes = this._movies.filter((movie: IMovie) => movie.year === year)
+
+            if (filmes.length === 0) throw new Error("Invalin films year");
+
+            return filmes;
+
+        } catch(err) {
+            if (err instanceof Error) throw err;
+            else throw new Error("Error during filtering");
+        }
+    }
+
+    public filterByGenre() {
+        
+    }
+
+    private _setMoviesData(): void {
         csv
             .fromFile(this._csvFilePath)
             .then((jsonData) => {
                 const filteredJsonData = jsonData.filter(i => i.id && i.name && i.genre && i.year);
                 this._movies = filteredJsonData;
+                return filteredJsonData;
+            })
+            .then((movies: IMovie[]) => {
+                this._genres = movies.reduce((acc, curr) => {
+                    const genres = curr.genre.split(',').map((item) => item.trim()).filter((item) => !acc.includes(item));
+                    return [...acc, ...genres];
+	            }, []);
             })
     }
 }
@@ -32,3 +91,5 @@ export interface IMovie {
     genre: string;
     year: string;
 }
+
+export type SortKey = keyof IMovie;
